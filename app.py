6536a97f38c6a1ebe1d3e6109483eef2b8286d62
DianaMukaliyeva <diana.mukaliyeva@gmail.com>
2020-05-14 19:23:29 +0300

    Implement endpoint '/movies'

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from models import setup_db
+from models import setup_db, Movie, Actor
 
 app = Flask(__name__)
 CORS(app)
@@ -11,6 +11,15 @@ setup_db(app)
 def index():
     return 'Hello there\n'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+@app.route('/movies')
+def show_movies():
+    movies = Movie.query.all()
+    response = [movie.format() for movie in movies]
+    return jsonify({
+        'movies': response
+    })
+
+
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=8080)
 
