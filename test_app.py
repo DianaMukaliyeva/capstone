d889e3eba6a40e2afcb1977e927e8068e442640f
DianaMukaliyeva <diana.mukaliyeva@gmail.com>
2020-05-15 14:06:25 +0300

    test: Setup test_app.py
    Setup the start for the tests.
    Created psql file for test_cases

---
+++ test_app.py
@@ -0,0 +1,44 @@
+import os
+import unittest
+import json
+
+from flask_sqlalchemy import SQLAlchemy
+
+from models import setup_db, Actor, Movie
+from app import create_app
+
+
+class CapstoneTestCase(unittest.TestCase):
+    '''This class represents the Capstone project test cases'''
+
+    def setUp(self):
+        '''Define test variables and initialize app.'''
+
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.db_name = 'capstone_test'
+        self.database_path = \
+            f'postgres://postgres:postgres@localhost:5432/{self.db_name}'
+        setup_db(self.app, self.database_path)
+
+        # Binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            self.db.create_all()
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    def test_get_movies(self):
+        res = self.client().get('/movies')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+
+'''Make the tests conveniently executable'''
+if __name__ == "__main__":
+    unittest.main()
